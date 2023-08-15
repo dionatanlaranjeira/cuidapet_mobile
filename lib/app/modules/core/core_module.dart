1b7e9ce70130fda5739cafe90734bbed7c4963b6
dionatanlaranjeira <dionatanribeiro.netrr@gmail.com>
2023-08-15 08:25:08 -0400

    encapsulamento shared preferences e flutter secure storage

--- lib/app/modules/core/core_module.dart
+++ lib/app/modules/core/core_module.dart
@@ -1,3 +1,6 @@
+import 'package:cuidapet/app/core/local_storage/flutter_secure_storage/flutter_secure_storage_local_storage_impl.dart';
+import 'package:cuidapet/app/core/local_storage/local_storage.dart';
+import 'package:cuidapet/app/core/local_storage/shared_preferences/shared_preferences_local_storage_impl.dart';
 import 'package:cuidapet/app/core/logger/app_logger.dart';
 import 'package:cuidapet/app/core/logger/logger_app_logger_impl.dart';
 import 'package:cuidapet/app/core/rest_client/dio/dio_rest_client.dart';
@@ -8,9 +11,25 @@ import 'package:flutter_modular/flutter_modular.dart';
 class CoreModule extends Module {
   @override
   List<Bind> get binds => [
-        Bind.lazySingleton((i) => AuthStore(), export: true),
-        Bind.lazySingleton<RestClient>((i) => DioRestClient(), export: true),
-        Bind.lazySingleton<AppLogger>((i) => LoggerAppLoggerImpl(),
-            export: true),
+        Bind.lazySingleton(
+          (i) => AuthStore(),
+          export: true,
+        ),
+        Bind.lazySingleton<RestClient>(
+          (i) => DioRestClient(),
+          export: true,
+        ),
+        Bind.lazySingleton<AppLogger>(
+          (i) => LoggerAppLoggerImpl(),
+          export: true,
+        ),
+        Bind.lazySingleton<LocalStorage>(
+          (i) => SharedPreferencesLocalStorageImpl(),
+          export: true,
+        ),
+        Bind.lazySingleton<LocalSecureStorage>(
+          (i) => FlutterSecureStorageLocalStorageImpl(),
+          export: true,
+        ),
       ];
 }
